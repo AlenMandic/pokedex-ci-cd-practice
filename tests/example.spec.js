d266d5ff2a91e0014635bace008ee07966eb504c
Alen Mandic <alenmandic1@gmail.com>
2024-06-07 18:19:20 +0200

    Added additional end-to-end test to workflow

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -6,4 +6,16 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('We can open individual pokemon pages and their details', async ({ page }) => {
+    await page.goto('http://localhost:8080/')
+
+    await page.click('text=Ivysaur') // click on individual pokemon
+
+    // Verifying the URL has changed
+    await expect(page).toHaveURL('http://localhost:8080/pokemon/ivysaur');
+
+    // Check if we are on Ivysaur page by checking for ability
+    await expect(page.getByText('Overgrow')).toBeVisible()
+  })
 })
